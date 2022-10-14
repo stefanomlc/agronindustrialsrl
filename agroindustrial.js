966d4db946e9271e30da1c531b957017dfd99e8d
Tato López <stefanolopez_1992@hotmail.com>
2022-10-14 18:10:10 -0300

    agregué la sección de contacto con vinculación a un correo nuevo, además de corregir alineaciones en otros productos

--- agroindustrial.js
+++ agroindustrial.js
@@ -1,3 +1,20 @@
-<script>
-  AOS.init();
-</script>
+const btn = document.getElementById('buttonContacto');
+
+document.getElementById('form')
+ .addEventListener('submit', function(event) {
+   event.preventDefault();
+
+   btn.value = 'Sending...';
+
+   const serviceID = 'service_s1wdmkn';
+   const templateID = 'template_sgzzivm';
+
+   emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      btn.value = 'Send Email';
+      alert('El correo fue enviado');
+    }, (err) => {
+      btn.value = 'Send Email';
+      alert(JSON.stringify(err));
+    });
+});
