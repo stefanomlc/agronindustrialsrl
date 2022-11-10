06c71dcd5405e2b78410ace8ee7731bc23085134
Tato López <stefanolopez_1992@hotmail.com>
2022-11-10 13:31:22 -0300

    modificación de la página total en footer y slider de testimonio

--- agroindustrial.js
+++ agroindustrial.js
@@ -18,3 +18,32 @@ document.getElementById('form')
       alert(JSON.stringify(err));
     });
 });
+
+
+
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 20
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 2,
+      spaceBetween: 30
+    },
+    // when window width is >= 640px
+    640: {
+      slidesPerView: 3,
+      spaceBetween: 40
+    }
+  }
+});
